47098bf3d8519c8d4605e3cf6c06473bf0eee174
Swapnil9693 <96917180+Swapnil9693@users.noreply.github.com>
2024-07-03 17:15:43 +0000

    data types in js

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,6 @@
+let userId=2000; //stores number data type of value
+let userName="Swapnil"; //stores string type of value
+let userHobby; // stores undefined value
+let userOccupation=null;// stores null type of value
+let userActive=true;// stores boolean type of data
+console.table([userId,userName,userHobby,userOccupation,userActive]);
