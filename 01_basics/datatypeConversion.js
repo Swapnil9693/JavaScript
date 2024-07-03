c5584f2e7d164d5214bc486dc76aed91811bb74a
Swapnil9693 <96917180+Swapnil9693@users.noreply.github.com>
2024-07-03 17:46:51 +0000

    datatype conversion

---
+++ 01_basics/datatypeConversion.js
@@ -0,0 +1,21 @@
+// (1)
+let valuee=true;
+let value1=Number(valuee);//'true' can be identified as '1' in the terms of Number 
+console.log(value1);
+// (2)
+let value2=0;
+let value22=Boolean(value2);//1 got converted in boolean type which is true similarly '0' will be false
+console.log(value22);
+// (3)
+let String1="33";
+let value33=Number(String1);// 33 got converted into number type as we tycasted it using Number datataype
+console.log(value33);
+// (4)
+let string2="swapnil";
+let value44=Boolean(string2);
+console.log(value44);
+// (5)
+let string3="";
+let value55=Boolean(string3);
+console.log(value55);
+
