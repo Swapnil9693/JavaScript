ca1e6508f88c65259f7903ea8999b12ee98dd839
Swapnil9693 <96917180+Swapnil9693@users.noreply.github.com>
2024-07-03 16:38:12 +0000

    javascript variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,7 @@
+const userId=1234786;
+let userName="Swapnil";
+var userEmail="swapnil@gmail.com";
+let userCity="pune"
+//userId=54378;//we cant redeclare a constant variable
+//dont use var as its not recommended because of its functional or block scope problem
+console.table([userId,userName,userEmail,userCity]);
