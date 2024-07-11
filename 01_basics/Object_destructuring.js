4d6cda8eaac8d1fa13d73901577aafaa2d252a96
Swapnil Dahiphale <96917180+Swapnil9693@users.noreply.github.com>
2024-07-11 16:40:51 +0000

    object destructure,alias,default value

--- 01_basics/Object_destructuring.js
+++ 01_basics/Object_destructuring.js
@@ -1,4 +1,5 @@
 let object1={
+    hobby:"cricket",
     name:"Swapnil",
     age:24,
     college:"SITS",
@@ -8,14 +9,15 @@ let object1={
 
 //console.log(object1.name);//normal property access method using dot operator
 
-let {name,age,city,college}=object1;// Object destructuring to access the object properties easily
-object1.name="pratik";// name property reassigned a value
-console.log(name);// change in object's property doesnt affect destructured variables 
-console.log(age);
-console.log(city);
+// let {name,age,city,college}=object1;// Object destructuring to access the object properties easily
+// object1.name="pratik";// name property reassigned a value
+// console.log(name);// change in object's property doesnt affect destructured variables 
+// console.log(age);
+// console.log(city);
 
-const{name:n,city:c,height:h}=object1;// we can also give aliases to properties to access it easily
+const{hobby="save people"/*(default value to be extracted in case no value assigned in original object)*/,name:n,city:c,height:h}=object1;// we can also give aliases to properties to access it easily
 console.log(n);
+console.log(hobby);
 console.log(c);
 console.log(h);
 console.log(object1.name);
