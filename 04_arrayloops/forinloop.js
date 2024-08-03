066e4c994eb6912f04e5664fc36e616562267788
Swapnil Dahiphale <96917180+Swapnil9693@users.noreply.github.com>
2024-08-03 13:07:24 +0000

    for in loop on object

---
+++ 04_arrayloops/forinloop.js
@@ -0,0 +1,17 @@
+//(1) for in loop on object
+const obj1={name:"swapnil",age:24,college:"SITS",city:"pune"};
+for(const item in obj1)
+{
+   // console.log(item);
+   //console.log(obj1[item]);
+   
+    
+}
+
+//(2) for in loop for array
+const array1=["mon","tue","wed","thu","fri","sat","sun"];
+for(const item in array1)
+{
+    console.log(array1[item]);
+     
+}
