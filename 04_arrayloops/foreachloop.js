e9999f878674998b72eec82e590f1715edf20ce7
Swapnil Dahiphale <96917180+Swapnil9693@users.noreply.github.com>
2024-08-04 07:45:19 +0000

    for each loop

---
+++ 04_arrayloops/foreachloop.js
@@ -0,0 +1,11 @@
+const array1=[10,20,30,40,50,60];
+const array2=array1.forEach((num)=>
+    {
+        console.log(num);
+        
+    }
+);
+console.log(array2);
+
+//note: (1) foreach loop iterates over each element of an array in ascending order.
+     // (2) it always returns undefined.
