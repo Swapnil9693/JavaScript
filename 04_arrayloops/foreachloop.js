94ad2e8e8d8284ebf228e0b61b354db4d253e29a
Swapnil Dahiphale <96917180+Swapnil9693@users.noreply.github.com>
2024-08-04 07:53:12 +0000

    for each loop for array and object

--- 04_arrayloops/foreachloop.js
+++ 04_arrayloops/foreachloop.js
@@ -9,3 +9,15 @@ console.log(array2);
 
 //note: (1) foreach loop iterates over each element of an array in ascending order.
      // (2) it always returns undefined.
+
+
+     // for each loop to iterate over an object
+     const arrayobj=[{name:"swapnil",age:24},{name:"pratik",age:23},{name:"pratik dahiphale",age:23},{name:"sandhya",age:25}];
+     const array3=arrayobj.forEach((item)=>{
+        console.log(item.name);
+        console.log(item.age);
+        
+        
+     });
+     console.log(array3);
+     
