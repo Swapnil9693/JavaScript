8768431d125d3a7042e2da13074e1baf8fa1e511
Swapnil Dahiphale <96917180+Swapnil9693@users.noreply.github.com>
2024-08-05 18:27:30 +0530

    Delete 06_dom/htmlcollection.js

--- 06_dom/htmlcollection.js
+++
@@ -1,8 +0,0 @@
-let collection=document.querySelectorAll('li');
-console.log(collection);
-
-const array1=Array.from(collection);// converting nodelist to an array as we cannot use forEach on node list
-array1.forEach((li)=>{
-  li.style.backgroundColor="lightblue";
-  li.style.margin="10px";
-})
