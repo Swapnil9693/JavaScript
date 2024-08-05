9f5b31b543fbfa093e0e8cc3c7e3212bff63dcc0
Swapnil Dahiphale <96917180+Swapnil9693@users.noreply.github.com>
2024-08-05 08:15:33 +0000

    4 methods done

--- 06_dom/dommethods.js
+++ 06_dom/dommethods.js
@@ -1,20 +1,25 @@
-//(1).getElementById() Method
-let i=document.getElementById('id-para');
-i.style.backgroundColor="red"; // sets the color of paragraph to blue;
-console.log(i); // returns element values
+// //(1).getElementById() Method
+// let i=document.getElementById('id-para');
+// i.style.backgroundColor="red"; // sets the color of paragraph to blue;
+// console.log(i); // returns element values
 
-//(2).getElementByClassName() Method
-let b=document.getElementsByClassName("heading");
+// //(2).getElementByClassName() Method
+// let b=document.getElementsByClassName("heading");
 
-for(let a=0;a<b.length;a++)
-{
-    b[a].style.backgroundColor="red";
-}
+// for(let a=0;a<b.length;a++)
+// {
+//     b[a].style.backgroundColor="red";
+// }
 
-//(3).querySelector() Method
-document.querySelector('.class-para').style.backgroundColor="lightblue"; // using class name
-document.querySelector('h1').style.backgroundColor="orange"; // using tag name
-document.querySelector('#id-para').style.backgroundColor="palegreen"; // using id name
+// //(3).querySelector() Method
+// document.querySelector('.class-para').style.backgroundColor="lightblue"; // using class name
+// document.querySelector('h1').style.backgroundColor="orange"; // using tag name
+// document.querySelector('#id-para').style.backgroundColor="palegreen"; // using id name
 
+//(4) document.querySelector().attribute
+let a1=document.querySelector('.class-para').getAttribute('id');
+console.log(a1); // output: id-para
+
+//document.querySelector('.class-para').getAttribute('id');
 
 
