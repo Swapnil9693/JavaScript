a2333960c68867825891304f066199dfd603182b
Swapnil Dahiphale <96917180+Swapnil9693@users.noreply.github.com>
2024-08-11 15:26:21 +0000

    promise

---
+++ 11_promise/promise.js
@@ -0,0 +1,25 @@
+const promiseOne= new Promise((resolve,reject)=>
+    {
+        setTimeout(function()
+    {   const status=true;
+        if(!status)
+        {
+            resolve("promise one resolved !");
+        }
+        else
+        {
+            reject("Error ho gaya hai");
+        }
+       
+    },2000);
+    })
+    .then((message)=>
+    {
+    console.log(message);
+    
+    })
+    .catch((error)=>
+    {
+    console.log(error);
+    
+    })
